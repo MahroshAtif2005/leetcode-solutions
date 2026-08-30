211464b1dd5bab04d09e0183bb9bc6b996de31b4
Mahrosh Atif <rosche.atif@gmail.com>
2026-08-30 15:14:13 +0400

    Time: 0 ms (100%), Space: 14.1 MB (80.01%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+       /* sort(nums.begin(),nums.end());
         return nums[0];
+        */
+        // we can't sort array coz it takes -> O(n log n) time
+        // but the question asks to use O(log n) time which means Binary Search
+        int left = 0;
+        int right = nums.size()-1;
+        while (left<right){
+            int mid = left + (right-left)/2;
+
+            if (nums[mid]>nums[right]){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return nums[left];
     }
 };
