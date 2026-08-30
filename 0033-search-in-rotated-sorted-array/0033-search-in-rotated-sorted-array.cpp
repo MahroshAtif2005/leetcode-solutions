f08a837c772556e6a5a95dcd6a09aa8d1ed2ce6f
Mahrosh Atif <rosche.atif@gmail.com>
2026-08-30 13:41:41 +0400

    Time: 0 ms (100%), Space: 15.2 MB (36.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -11,26 +11,21 @@ public:
           if(target==nums[mid]){
             return mid;
           }
-
-          // LEFT is sorted
           if(nums[left]<=nums[mid]){
-            //target lies in the left half
-            if(nums[left]<=target && target<nums[mid]){
-                right = mid-1;
+            if(nums[left]<= target && target<nums[mid]){
+                right = mid -1;
             }
             else{
-                left = mid+1;
+                left = mid +1;
             }
-          }
-        else{
-          // RIGHT is sorted
-          if(nums[mid]<target && target<=nums[right]){
-            // if target lies in right half
-              left = mid+1;
-            }else{
-                right = mid -1;
-            } 
-           }
+            }
+            else{
+                if(nums[right]>=target && nums[mid]<target){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }       
         }
         return -1;
     }
