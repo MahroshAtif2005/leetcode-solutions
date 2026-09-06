0313234ee04e364a446159d984cf96092bc16912
Mahrosh Atif <rosche.atif@gmail.com>
2026-09-06 18:01:55 +0400

    Time: 20 ms (63.88%), Space: 105.5 MB (56.98%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        //monotonic stack
-        vector<int> stack;
-        //creating the answer vector already filled with zeros.
-        //vector<int> name(size, initialValue);
-        //Im storing indices on the stack not the value
-        vector<int> answer(temperatures.size(),0);
-      
-        for (int i = 0; i<temperatures.size(); i++){
-            while(!stack.empty()&& temperatures[i]>temperatures[stack.back()]){
-                int prevIndex=stack.back();
-                stack.pop_back();
-                answer[prevIndex]=i-prevIndex;
-            }
-              stack.push_back(i); 
+       vector<int> stack;
+       vector<int> answer(temperatures.size(),0);
+       for (int i = 0;i<temperatures.size();i++){
+        while(!stack.empty() && temperatures[i]>temperatures[stack.back()]){
+            int prevIndex = stack.back();
+            stack.pop_back();
+            answer[prevIndex]=i-prevIndex;
         }
-        return answer;
+        stack.push_back(i);
+       }
+       return answer;
     }
 };
