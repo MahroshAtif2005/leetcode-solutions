2a1596b7bfb5209cb343e20ed390758b0a6c9afa
Mahrosh Atif <rosche.atif@gmail.com>
2026-09-08 15:36:21 +0400

    Time: 70 ms (21.96%), Space: 107.5 MB (44.99%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+        vector<pair<int,int>> cars;
+        for (int i = 0;i<position.size();i++){
+            cars.push_back({position[i],speed[i]});
+        }
+        sort(cars.rbegin(),cars.rend());
+
+        int fleets = 0;
+        double slowestTime = 0;
+
+        for (auto car: cars){
+            double time = (double) (target-car.first)/car.second;
+            if(time>slowestTime){
+                fleets++;
+                slowestTime=time;
+            }
+        }
+        return fleets;
+    }
+};
