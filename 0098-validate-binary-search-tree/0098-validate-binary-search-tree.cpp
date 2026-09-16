9b4b0d17e7fe48ee7f42524c0ae1b83668f88645
Mahrosh Atif <rosche.atif@gmail.com>
2026-09-16 19:38:20 +0400

    Time: 0 ms (100%), Space: 21.8 MB (94.85%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -13,21 +13,15 @@ class Solution {
 public:
 
     bool valid(TreeNode* node, long long minimum, long long maximum){
-        if (node==nullptr){
-            return true;
-        }
-        //current node must be inside its allowed range
-        if(node->val<=minimum || node->val>=maximum){
-            return false;
-        }
-        //check both subtrees
-        return valid(node->left,minimum,node->val) && valid(node->right,node->val,maximum);
+      if(node==nullptr){
+        return true;
+      }
+      if(node->val<=minimum || node->val>=maximum){
+        return false;
+      }
+      return valid(node->left,minimum,node->val) && valid(node->right,node->val,maximum);
     }
     bool isValidBST(TreeNode* root) {
-        //dfs 
-        //everything left of subtree should be less than root
-        //and everything at right of subtree should be more than root and so on
-        //What RANGE of values is this node allowed to have?
      return valid(root,LLONG_MIN,LLONG_MAX);
     }
 };
