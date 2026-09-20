22c1bb2020c0dc4d6d0dadbc3b17748baf52ce38
Mahrosh Atif <rosche.atif@gmail.com>
2026-09-20 15:54:11 +0400

    Time: 0 ms (100%), Space: 18.3 MB (27.15%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    struct compare{
-        bool operator()(const pair<int,string>& a,const pair<int,string>& b){
-           if(a.first==b.first){
-            return a.second<b.second;
-           }
-           return a.first>b.first;
+    struct Compare{
+        bool operator()(const pair<string,int>& a, const pair<string,int>& b){
+            if(a.second==b.second){
+                return a.first<b.first;
+            }
+            return a.second>b.second;
         }
     };
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int> map;
-        for (string word: words){
-                map[word]++;
+      unordered_map<string,int> freq;
+      for(string word:words){
+        freq[word]++;
+      }
+      priority_queue<pair<string,int>,vector<pair<string,int>>,Compare> pq;
+
+      for(auto element : freq){
+        pq.push({element.first,element.second});
+        if(pq.size()>k){
+            pq.pop();
         }
-         priority_queue<pair<int,string>,vector<pair<int,string>>,compare> pq;
-          for (auto element : map){
-            pq.push({element.second, element.first});
-            if(pq.size()>k){
-                pq.pop();
-            }
-          }
-          vector<string> answer;
-          //higher freq comes first
-          //same freq->sort in lexgraphically sorted order
-        while(!pq.empty()){
-          answer.push_back(pq.top().second);
-          pq.pop();
-        }
-        reverse(answer.begin(),answer.end());
-        return answer;
+      }
+      vector<string> answer;
+      while(!pq.empty()){
+       answer.push_back({pq.top().first});
+       pq.pop();
+      }
+
+      reverse(answer.begin(),answer.end());
+      return answer;
     }
 };
