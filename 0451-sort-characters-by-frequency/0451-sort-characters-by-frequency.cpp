5acec138cd29ce17c485f47c6415d4d62fb3c05c
Mahrosh Atif <rosche.atif@gmail.com>
2026-09-21 21:49:40 +0400

    Time: 0 ms (100%), Space: 10.8 MB (77.69%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -7,27 +7,19 @@ public:
         for (int i =0;i<s.size();i++){
             map[s[i]]++;
         }
-
-        //create a pair array and store hashmap elements on it
-        vector<pair<int,char>> freq;
-
-        //we put freq first and then the value 
-        for (auto &pair : map){
-           freq.push_back({pair.second,pair.first});
+        priority_queue<pair<int,char>> pq;
+        for(auto element : map){
+            pq.push({element.second,element.first});
         }
-
-        //sort in decending order
-        sort(freq.rbegin(),freq.rend());
-
-        //create a string which we will return
-        string ans;
-
-        for (int i = 0; i<freq.size();i++){
-          for (int j = 0; j<freq[i].first;j++){
-          ans+= freq[i].second;
-          }
+        string answer;
+        while(!pq.empty()){
+            int frequency = pq.top().first;
+            char character = pq.top().second;
+            for (int i= 0; i< frequency; i++){
+                answer+= character;
+            }
+            pq.pop();
         }
-        return ans;
-        
+        return answer;
     }
 };
