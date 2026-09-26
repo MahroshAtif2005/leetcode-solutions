1ff334c57647d8c413f5bb15df804506ff22a89c
Mahrosh Atif <rosche.atif@gmail.com>
2026-09-26 17:10:06 +0400

    Time: 0 ms (100%), Space: 10 MB (97.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    int robRange(vector<int>& nums, int start, int end){
+        //using pointers instead of dp array to save space
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i<=end; i++){
+            int current = max(prev1,nums[i]+prev2);
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
     int rob(vector<int>& nums) {
-        //same as house robber I but since its cyclic, check if the last is selected,
+       //same as house robber I but since its cyclic, check if the last is selected,
        // first shouldnt be and if first is then last shouldnt be
        int n = nums.size();
        if(n == 1){
         return nums[0];
        }
-   
-       vector<int> dp1(n,0);
-       dp1[0]= nums[0];
-       dp1[1]=max(nums[0],nums[1]);
-       //case 1: ignore the last house
-       for (int i = 2; i<=n-2; i++){
-         dp1[i]= max(dp1[i-1],nums[i]+dp1[i-2]);
-       }
 
-       //case 2: ignore the first house
-       vector<int> dp2(n,0);
-       dp2[1]=nums[1];
-       for (int j = 2;j<=n-1;j++){
-          dp2[j]= max(dp2[j-1],nums[j]+dp2[j-2]);
-       }
-       
-       return max(dp1[n-2],dp2[n-1]);
+       return max(
+        robRange(nums,0,n-2),//ignore last
+        robRange(nums,1,n-1)//ignore first
+       );
     }
 };
